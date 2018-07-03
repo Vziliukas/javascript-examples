42f6d0d0dc02a72cfac3ccdb40ed932c90c209b5
Vytautas Ziliukas <>
2018-07-03 13:23:24 +0300

    Triangle patterns algorithms

---
+++ triangles-ca/script.js
@@ -0,0 +1,68 @@
+const container = document.querySelector(".container p");
+const btns = document.querySelectorAll("button");
+const countInput = document.getElementById('count');
+
+const createTriangle = (rows) => {
+  let triangle = "";
+
+  for (let i = 0; i <= rows; i++) {
+    for (let j = 0; j < i; j++) {
+      triangle += "*&nbsp";
+    }
+    triangle += "<br>";
+  }
+  container.innerHTML = triangle;
+};
+
+const createPyramid = (rows) => {
+  let pyramid = "";
+
+  for (let i = 0; i < rows; i++) {
+    for (let k = 0; k < rows - i; k++) {
+      pyramid += "&nbsp";
+    }
+    for (let j = 0; j < i; j++) {
+      pyramid += "*&nbsp";
+    }
+    pyramid += "<br>";
+  }
+  container.innerHTML = pyramid;
+};
+
+const createRevPyramid = (rows) => {
+  let revPyramid = "";
+
+  for (let i = rows; i > 0; i--) {
+    for (let k = 0; k < rows - i; k++) {
+      revPyramid += "&nbsp";
+    }
+    for (let k = 0; k < i; k++) {
+      revPyramid += "*&nbsp";
+    }
+    revPyramid += "<br>";
+  }
+  container.innerHTML = revPyramid;
+};
+
+const clear = () => container.innerHTML = "";
+
+function draw(action, rows) {
+  switch (action) {
+    case "triangle":
+      createTriangle(rows);
+      break;
+    case "pyramid":
+      createPyramid(rows);
+      break;
+    case "reverse pyramid":
+      createRevPyramid(rows);
+      break;
+    case "clear":
+      clear();
+      break;
+  }
+}
+
+btns.forEach(btn =>
+  btn.addEventListener("click", e => draw(e.target.innerHTML, countInput.value))
+);
