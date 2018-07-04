249f51c483d15ad59d70d8a5c13f7fa80e13f38e
Vytautas Ziliukas <>
2018-07-04 18:45:08 +0300

    business card app

---
+++ business-card/script.js
@@ -0,0 +1,38 @@
+(doc => {
+  const form = doc.querySelector("form");
+  const section = doc.querySelector("section");
+  const inputs = doc.querySelectorAll("input, textarea");
+  const names = Array.from(inputs).map(input => input.name);
+
+  const renderCard = details => {
+    let list = "";
+
+    Object.entries(details).forEach(detail => {
+      list += `<li class="card-item">${detail[0]}: ${detail[1]}</li>`;
+    });
+    section.innerHTML = `<ul class="card-list">${list}</ul>`;
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const details = {};
+    names.forEach(name => (details[name] = e.target[name].value));
+
+    Object.entries(details).forEach(detail => {
+      localStorage.setItem(detail[0], detail[1]);
+    });
+    renderCard(details);
+  };
+
+  const initialize = () => {
+    if (localStorage.length) {
+      const details = {};
+      names.forEach(name => (details[name] = localStorage.getItem(name)));
+
+      renderCard(details);
+    }
+  };
+
+	initialize();
+  form.addEventListener("submit", e => handleSubmit(e));
+})(document);
